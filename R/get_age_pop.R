d60ec5f0e575f1f8592f25dab8c761b432c7c53c
patrickbarks <patrick.barks@gmail.com>
2020-05-19 11:28:08 +0200

    clean up code and doc

--- R/get_age_pop.R
+++ R/get_age_pop.R
@@ -1,13 +1,22 @@
-#' Get population distribution and aggregate it to 10 year age groups
+#' Extract age-specific population estimates for a given country from the UN
+#' World Population Prospects 2019
 #'
-#' This is set up to use population estimates from the World Populaiton
-#' Prospects data
+#' Extract population estimates in 10-year age intervals for a given country
+#' from the UN World Population Prospects 2019 (WPP2019).
 #'
-#' @param iso country of interest
-#' @param format Data frame format to return ("wide" or "long"). "wide" yields a
+#' @param iso ISO3 code for country of interest
+#' @param format Return format ("wide" or "long"). Format "wide" yields a
 #'   single-row data.frame with age classes as column names, whereas "long"
-#'   yields a tidy 2-column data.frame with the first defining age classes and
-#'   the second giving corresponding population sizes. Defaults to "wide".
+#'   yields a 2-column data.frame with the first defining age classes and the
+#'   second giving corresponding population sizes. Defaults to "wide".
+#'
+#' @return
+#' A data.frame. If \code{format = "wide"} returns a single-row data.frame with
+#' age classes as column names, whereas if \code{format = "long"} returns a
+#' 2-column data.frame with the first defining age classes and the second giving
+#' corresponding population sizes.
+#'
+#' @source \url{https://population.un.org/wpp/Download/Standard/Population/}
 #'
 #' @examples
 #' get_age_pop(iso = "FRA")
@@ -16,40 +25,31 @@
 #' @export get_age_pop
 get_age_pop <- function(iso, format = c("wide", "long")) {
 
-
   format <- match.arg(format)
 
-  if (FALSE) {
-    iso <- "USA"
-    format <- "long"
-  }
+  # fetch WPP2019 dataset
+  dat <- get_pop_data()
 
-  pop_data <- get_pop_data()
+  # subset to country and max year, and select only age-population columns
+  pop_cols <- c("0-9", "10-19",
+                "20-29", "30-39",
+                "40-49", "50-59",
+                "60-69", "70-79",
+                "80-89", "90-99",
+                "100-109")
 
-  # subset to country and max year
-  pop_data <- pop_data[pop_data$iso_a3 %in% iso,]
-  pop_data <- pop_data[pop_data$year == max(pop_data$year, na.rm = TRUE),]
+  dat <- dat[dat$iso_a3 %in% iso,]
+  dat <- dat[dat$year == max(dat$year, na.rm = TRUE), pop_cols]
 
-  # print for a double check
-  # print(pop_data$location)
-  out <- pop_data[,c("0-9",
-                     "10-19",
-                     "20-29",
-                     "30-39",
-                     "40-49",
-                     "50-59",
-                     "60-69",
-                     "70-79",
-                     "80-89",
-                     "90-99",
-                     "100-109")]
+  # strip rownames
+  rownames(dat) <- NULL
 
   # output format
   if (format == "long") {
-    out <- data.frame(age_group = names(out),
-                      pop = as.numeric(out[1,]),
+    dat <- data.frame(age_group = names(dat),
+                      pop = as.numeric(dat[1,]),
                       stringsAsFactors = FALSE)
   }
 
-  return(out)
+  return(dat)
 }
